460de0ddb00868f66f418fd9b20d05db0c317c11
joehans <joehans759@sohu.com>
2017-12-03 22:11:58 +0800

    添加 修改页面 thymeleaf模板

--- src/main/java/com/joe/ems/controller/EmployeeController.java
+++ src/main/java/com/joe/ems/controller/EmployeeController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.joe.ems.dao.DepartmentMapper;
 import com.joe.ems.dao.EmployeeMapper;
+import com.joe.ems.domain.Department;
 import com.joe.ems.domain.Employee;
 
 @RequestMapping("employee")
@@ -16,11 +19,25 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeMapper employeeMapper;
+	@Autowired
+	private DepartmentMapper departmentMapper;
 	
 	@RequestMapping("list")
 	public void list(Model model) {
 		List<Employee> employees = employeeMapper.select();
 		model.addAttribute("employees", employees);
+	}
+	
+	@RequestMapping("toAdd")
+	public void toAdd() {
+	}
+	
+	@RequestMapping("toEdit")
+	public void toEdit(@RequestParam("id")Integer id, Model model) {
+		Employee employee = employeeMapper.selectByPrimaryKey(id);
+		model.addAttribute("employee", employee);
+		List<Department> departments = departmentMapper.select();
+		model.addAttribute("departments", departments);
 	}
 	
 }
