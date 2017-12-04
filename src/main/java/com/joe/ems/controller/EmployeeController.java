20dcf4bbf4b42b8e9cbc22a071b93519e6a7bf72
joe <jiaozhun@sina.com>
2017-12-04 20:50:43 +0800

    正在添加功能验证（未实现）

--- src/main/java/com/joe/ems/controller/EmployeeController.java
+++ src/main/java/com/joe/ems/controller/EmployeeController.java
@@ -2,12 +2,11 @@ package com.joe.ems.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +32,24 @@ public class EmployeeController {
 	}
 	
 	@RequestMapping("toAdd")
-	public void toAdd() {
+	public void toAdd(Model model) {
+		List<Department> departments = departmentMapper.select();
+		model.addAttribute("departments", departments);
+	}
+	
+	@RequestMapping("add")
+	public String add(@ModelAttribute("employee")Employee employee,BindingResult result, Model model) {
+		model.addAttribute("employee", employee);
+		if(result.hasErrors()) {
+			for (ObjectError error : result.getAllErrors()) {
+				System.out.println(error.getObjectName()+":"+error.getDefaultMessage());
+			}
+			return "add";
+		} else {
+			return "redirect:/employee/list";			
+		}
+		
+		
 	}
 	
 	@RequestMapping("toEdit")
